e5bf3da49423700730965b12dfcda362537ea41a
rageCode153 <ChrisKonstantopoulos2002@gmail.com>
2021-03-15 23:47:28 +0200

    Removed unnecessary directives

--- Stream Counter/Settings.cs
+++ Stream Counter/Settings.cs
@@ -2,13 +2,6 @@
 using System.IO;
 using Newtonsoft.Json;
 using Microsoft.WindowsAPICodePack.Dialogs;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Stream_Counter
